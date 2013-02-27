d19e576e09a2081a0cdc9430d714704029cff7a8
Jonathan Kleid <jonathan.kleid@gmail.com>
2013-02-27 04:46:25 +0000

    First attempt, ex10 solution 1

---
+++ 1_3_2/ex10sol1.c
@@ -0,0 +1,37 @@
+
+int* Saddle(int matrix[9*8])
+{
+  int list[8];
+  int idx; // r1
+  int col; // r2
+  int pos; // r3
+  int elem; // rX
+  int* a10 = matrix - sizeof(int);
+  idx = 9*8;     // START
+  do {           // ROWMIN
+    col = 8;
+    do {
+      elem = a10[idx];     // 2H
+      pos = 0;
+      do {
+        list[pos++] = col; // 4H
+        do {
+          idx--;           // 1H
+          if (--col == 0)
+            goto COLMAX;
+        } while(elem < a10[idx]); // 3H
+      } while(elem == a10[idx]);
+    } while(elem > a10[idx]);
+COLMAX: // At this point, elem is the minimum value of the row just considered.
+    do {
+      col = list[pos-1];
+      col += 9*8-8;
+      do {
+        if (elem < a10[col])  // 1H
+          goto NO;
+      } while((col-=8) > 0);
+      return &a10[col+8];     // YES
+NO:
+    } while (--pos > 0);
+  } while (idx > 0);
+}
